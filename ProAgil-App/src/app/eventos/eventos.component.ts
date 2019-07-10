2f69d3793eef7aef99de687dff567cf875580a10
regithub21 <renan.cont20@yahoo.com.br>
2019-07-10 17:12:00 -0300

    Versão até aula 52

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -9,29 +9,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
-    eventos: any;
-  // eventos: any = [
-  //   {
-  //     EventoId: '1',
-  //     Tema: 'Angular',
-  //     Local: 'Belo Horizonte'
-  //   },
-  //   {
-  //     EventoId: '2',
-  //     Tema: '.NET Core',
-  //     Local: 'São Paulo'
-  //   },
-  //   {
-  //     EventoId: '3',
-  //     Tema: 'Angular e Dot Net',
-  //     Local: 'Rio de Janeiro'
-  //   }
-  // ];
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+    eventosFiltrados: any = [];
+    eventos: any = [];
+
+    imagemLargura = 50;
+    mostrarImagem = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
   }
 
   getEventos() {
